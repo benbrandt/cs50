7bb02ccc177c3f924be2a6bfe8a06c0f76b0b73f
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-24 13:54:45 -0400

    If/else statement with user input

---
+++ week-1/conditions-0.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Please give me an int: ");
+    int n = GetInt();
+    
+    if (n > 0)
+    {
+        printf("You picked a positive integer!\n");
+    }
+    else
+    {
+        printf("You picked a negative integer!\n");
+    }
+}
