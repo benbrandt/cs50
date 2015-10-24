1f61eeaed171e3722d677c3ec0bd96f4bac9277d
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-10-24 14:27:21 -0400

    Get sizes of various types in C

---
+++ week-1/sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    double d;
+    float f;
+    int i;
+    
+    printf("char: %i\n", sizeof(c));
+    printf("double: %i\n", sizeof(d));
+    printf("float: %i\n", sizeof(f));
+    printf("int: %i\n", sizeof(i));
+}
