af9c4ab16bf1f4858c0942facedad0db3d9c0f8f
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-11-02 00:49:57 -0500

    Print Mario pyramid based on user input

---
+++ pset1/mario.c
@@ -0,0 +1,37 @@
+// Program to recreate pyramid from Super Mario Bros - World 1-1
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, i, j, k;
+    
+    // Get height of pyramid from user
+    do
+    {
+        printf("Height: ");
+        height = GetInt();
+    }
+    while (height < 0 || height > 23);
+    
+    // Print lines
+    for (i = 1; i <= height; i++)
+    {
+        // Print spaces
+        for (j = 0; j < (height - i); j++)
+        {
+            printf(" ");
+        }
+        
+        // print hashes
+        for (k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
