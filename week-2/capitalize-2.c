ad43e1198c146120cae64b7002ed9a8944cf3835
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-11-08 00:13:06 -0500

    capitalize-2 remove if statement because toupper handles case

---
+++ week-2/capitalize-2.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = GetString();
+    
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
+    printf("\n");
+}
