aae732077e5cc88124610101b1a27abfc5c1efc8
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-11-07 22:13:44 -0500

    string-0 strings as arrays of chars

---
+++ week-2/string-0.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = GetString();
+    
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c\n", s[i]);
+    }
+}
