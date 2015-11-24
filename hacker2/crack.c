b86c423b9a9e72c83531ea680c9cc10015c8ce08
Ben Brandt <benjamin.j.brandt@gmail.com>
2015-11-24 12:55:38 -0800

    Update crack.c to brute force passwords less than 8 characters

--- hacker2/crack.c
+++ hacker2/crack.c
@@ -8,17 +8,17 @@
 #include <unistd.h>
 
 int main(int argc, string argv[])
-{   
+{
     void incrementChar(char string[], int index);
 
     // Check if correct # of arguments given
     if (argc != 2)
     {
         printf("Wrong number of arguments. Please try again.\n");
-        
+
         return 1;
     }
-    
+
     // Get Salt
     char salt[3];
     strncpy(salt, argv[1], 2);
@@ -27,51 +27,58 @@ int main(int argc, string argv[])
     // Load dictionary of words
     FILE *dict = fopen("/usr/share/dict/words", "r");
     char word[255];
-    
+
     // Check if file loads, otherwise return error
     if (dict == NULL)
     {
         printf("Error opening file");
-        
+
         return 1;
     }
-    
+
     // Loop through file to get strings
     while (fgets(word, 255, (FILE*)dict) != NULL)
     {
         // Check if word is 8 chars and if running crypt on word equals input
-        if (strlen(word) == 9 && strncmp(crypt(word, salt), argv[1], 8) == 0)
+        if (strlen(word) <= 9 && strcmp(crypt(word, salt), argv[1]) == 0)
         {
             // Print password
             printf("%s", word);
-                
-            return 0;   
-        }
-    }
-    
-    // Close file
-    fclose(dict);
-    
-    // If not in the dictionary, brute force it
-    char test[9] = "!!!!!!!!\0";
-    
-    // Increment string
-    while (test[7] < '~')
-    {
-        if (strncmp(crypt(test, salt), argv[1], 8) == 0)
-        {
-            // Print password
-            printf("%s", word);
-            
+
             return 0;
         }
-        
-        // Increment letter
-        incrementChar(test, 0);
     }
+
+    // Close file
+    fclose(dict);
+
+    // If not in the dictionary, brute force it
+    // Initialize string to all null characters
+    char test[9] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
+
+    // Increment string    
+    for (int i = 0; i < 8; i++)
+    {
+        test[i] = '!';
+        
+        while (test[i] <= '~')
+        {
+            // Compares until next null character
+            if (strcmp(crypt(test, salt), argv[1]) == 0)
+            {
+                // Print password
+                printf("%s\n", test);
     
+                return 0;
+            }
+            
+            // Increment letter
+            incrementChar(test, 0);
+        }
+    }
+
     printf("Password not found\n");
-    
+
     return 0;
 }
 
@@ -81,7 +88,7 @@ void incrementChar(char string[], int index)
     if (string[index] == '~')
     {
         string[index] = '!';
-        
+
         if (index < 7)
         {
             incrementChar(string, index + 1);
