a7fa9f237c57f2c26d1e8a1df741a1fe88df24ca
Benjamin Brandt <benjamin.j.brandt@gmail.com>
2016-01-10 23:24:52 +0000

    Pull in pset3 files and add comments to generate file

---
+++ pset3/find/generate.c
@@ -0,0 +1,57 @@
+/**
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Ensure the proper amount of command line arguments are passed in
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+    // Set first argument number of random integers to return
+    int n = atoi(argv[1]);
+
+    // If seed is present, it sets the seed to that value, otherwise it uses
+    // the current time as a seed value
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    // Return n random numbers of value between 0 and 1 and multiply by the 
+    // defined limit of the program
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
