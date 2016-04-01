0b2132f733ec5f382ac0932d2823082ad8f14f1c
Benjamin Brandt <benjamin.j.brandt@gmail.com>
2016-04-01 21:30:32 +0000

    Create file to recover deleted files on memory card

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -7,7 +7,59 @@
  * Recovers JPEGs from a forensic image.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char* argv[])
 {
-    // TODO
+    // open memory card file
+    FILE* inptr = fopen("card.raw", "r");
+    if (inptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not open card.raw");
+        return 1;
+    }
+    
+    // Allocate memory for 512 byte chunks
+    unsigned char buffer[512];
+    
+    // How many jpgs found
+    int counter = 0;
+    
+    // Current filename and img
+    char title[10];
+    FILE* img;
+    
+    // Read until end of card
+    while(fread(&buffer, 512, 1, inptr)) 
+    {
+        // start of new jpg?
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && 
+            buffer[2] == 0xff && buffer[3] >> 4 == 0xe)
+        {
+            // Found jpg
+            counter++;
+            
+            // Close prev file (if any)
+            if (counter > 1)
+            {
+                fclose(img);
+            }
+            
+            // Open new file and write first buffer
+            sprintf(title, "%03d.jpg", counter - 1);
+            img = fopen(title, "a");
+        }
+        
+        // If currently writing to file, write buffer
+        if (counter > 0)
+        {
+            fwrite(&buffer, 512, 1, img);    
+        }
+    }
+    
+    // close any remaining files
+    fclose(img);
+    fclose(inptr);
 }
