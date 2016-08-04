6b3500665d293337a82cdbd797ee6046431c5cce
Benjamin Brandt <benjamin.j.brandt@gmail.com>
2016-08-04 00:00:28 +0000

    mvc-5

---
+++ week-8/mvc-5/public/lectures.php
@@ -0,0 +1,10 @@
+<?php require(__DIR__ . "/../includes/helpers.php"); ?>
+
+<?php render("header", ["title" => "Lectures"]); ?>
+
+<ul>
+    <li><a href="week0.php">Week 0</a></li>
+    <li><a href="week1.php">Week 1</a></li>
+</ul>
+
+<?php render("footer"); ?>
