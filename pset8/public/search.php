23de17d5df79509fe227225aa57178d3f891b015
Benjamin Brandt <benjamin.j.brandt@gmail.com>
2016-12-30 21:39:14 +0000

    Add search results

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -4,8 +4,12 @@
 
     // numerically indexed array of places
     $places = [];
+    
+    // add wildcard to search terms
+    $query = str_replace(" ", "* ", $_GET["geo"]);
 
-    // TODO: search database for places matching $_GET["geo"], store in $places
+    // search database for places matching $_GET["geo"], store in $places
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1, admin_code1) AGAINST (? IN BOOLEAN MODE)", $query);
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
